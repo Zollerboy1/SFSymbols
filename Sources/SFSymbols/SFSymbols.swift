2b386cf0ec15c55d53d21fc2aaf325a6ee464226
Josef Zoller <josef@walterzollerpiano.com>
2019-06-06 14:17:56 +0200

    Added first image name

--- Sources/SFSymbols/SFSymbols.swift
+++ Sources/SFSymbols/SFSymbols.swift
@@ -1,3 +1,12 @@
-struct SFSymbols {
-    var text = "Hello, World!"
+import SwiftUI
+
+public enum SFSymbol: String {
+    case photo = "photo"
+}
+
+@available(iOS 13.0, watchOS 6, tvOS 13, *)
+public extension Image {
+    init(symbol: SFSymbol) {
+        self.init(systemName: symbol.rawValue)
+    }
 }
--- Tests/SFSymbolsTests/SFSymbolsTests.swift
+++ Tests/SFSymbolsTests/SFSymbolsTests.swift
@@ -3,10 +3,7 @@ import XCTest
 
 final class SFSymbolsTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SFSymbols().text, "Hello, World!")
+        
     }
 
     static var allTests = [
